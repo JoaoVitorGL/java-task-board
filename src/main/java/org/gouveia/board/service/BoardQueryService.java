738a40c9cbf882fe0d66fc005edb689bc59a34d4
JoaoVitorGl <vitorgouveia06@gmail.com>
2025-03-17 21:24:14 -0300

    feat: add entities, board dao, board service

---
+++ src/main/java/org/gouveia/board/service/BoardQueryService.java
@@ -0,0 +1,28 @@
+package org.gouveia.board.service;
+
+import lombok.AllArgsConstructor;
+import org.gouveia.board.persistence.dao.BoardColumnDAO;
+import org.gouveia.board.persistence.dao.BoardDAO;
+import org.gouveia.board.persistence.entity.BoardEntity;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class BoardQueryService
+{
+    private final Connection connection;
+
+    public Optional<BoardEntity> findById(final Long id) throws SQLException {
+        var dao = new BoardDAO(connection);
+        var boardColumnDAO = new BoardColumnDAO(connection);
+        var optional = dao.findById(id);
+        if (optional.isPresent()) {
+            var entity = optional.get();
+            entity.setBoardColumns(boardColumnDAO.findByBoardId(entity.getId()));
+            return Optional.of(entity);
+        }
+        return Optional.empty();
+    }
+}
